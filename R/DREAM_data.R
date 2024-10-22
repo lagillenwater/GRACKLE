0a20d9cf7bc2b3f91d8e2471484f6c4903aa6f9e
lagillenwater <lagillenwater@gmail.com>
2024-10-22 11:48:26 -0600

    adding function for aggregating simulation expression

--- R/DREAM_data.R
+++ R/DREAM_data.R
@@ -1,8 +1,25 @@
 ## These are functions for  aggregating the DREAM4 simulated data sets for testing GRACKLE.
 
 AggregateExpression <- function(training_directory, perturbation = NULL) {
-    if(is.null(perturbation)){
+    ## find the files in the directory
+    dirs <- list.dirs(training_directory, full.names = TRUE, recursive = FALSE)
+
+    for(dir in dirs) {
+        files <- list.files(dir)
+        dir_exp <- list()
         
+        if(is.null(perturbation)){
+            files <- files[grepl("knockdown|knockout|wildtype|multifactorial", files)]
+        } else {
+            files <- files[grepl(perturbation, files)]
+        }
+
+        for(file in files) {
+            file_name <- paste0(dir,"/",file)
+            single_exp <- read.delim(file_name)
+            print(head(single_exp))
         }
     }
+    
+}
 
